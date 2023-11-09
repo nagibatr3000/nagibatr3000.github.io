ea1a5af97a8d15e7e315e1001186e1d228f735ab
nagibatr3000 <132460378+nagibatr3000@users.noreply.github.com>
2023-11-09 21:24:03 +0300

    OMG NEW SITE!!
    DD

---
+++ 3Digruha/java/main.js
@@ -0,0 +1,21 @@
+var str = "";
+var token = "";
+function get_vars(){
+    str = document.getElementById("name").value;
+    token = document.getElementById("token").value;
+}
+function send(){
+    get_vars();
+    const msg = {
+        "content": str,
+     //   "username": false
+    };
+    console.log(msg)
+    if(str == ""){
+        console.log("error bruh")
+        return false;
+    }
+   
+    fetch(token + "?wait=true", {"method":"POST", "headers": {"content-type": "application/json"}, "body": JSON.stringify(msg)});
+
+} 
